aaea338edaafc96cf598c473435770d25bea47fd
bruno <bruno.b.fernandes12@hotmail.com>
2025-06-05 16:27:52 -0300

    fix: aplicando corretamente os estilos globais com styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.ts'
+import GlobalStyle from './global'
 import theme from './theme/mainColors'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Header />
       <Hero />
       <div className="container">
