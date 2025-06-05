faa72ae73a499439ed64113c1d02ec29161c111f
bruno <bruno.b.fernandes12@hotmail.com>
2025-06-04 23:36:56 -0300

    Convertendo estilos para styled-components e aplicando ThemeProvider global

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
+import { ThemeProvider } from 'styled-components'
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+import './global.ts'
+import theme from './theme/mainColors'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Header />
       <Hero />
       <div className="container">
         <ListaVagas />
       </div>
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ src/components/Cabecalho/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Header = styled.header`
+  background-color: ${(props) => props.theme.cores.secundaria};
+  color: ${(props) => props.theme.cores.principal};
+  text-align: center;
+  padding: 24px 0;
+`
